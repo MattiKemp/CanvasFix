345c80f274449d93c49e0597442294f6a04d2c5e
root <root@DESKTOP-68UL55P.localdomain>
2021-03-06 00:34:49 -0600

    added options in the drop down menu and automatic formatting when a page loads

--- content.js
+++ content.js
@@ -57,3 +57,48 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
         // });
     }
 })
+
+function fix(){
+    //add storage for settings 
+
+    //fix wrapper max width
+    var wrapper = document.getElementById('wrapper');
+    wrapper.style = "max-width: 100%;";
+
+    //get rid of top bar
+    var topBar = document.getElementsByClassName('ic-app-nav-toggle-and-crumbs no-print');
+    //console.log(topBar[0].innerHTML); 
+    //topBar[0].innerHTML = "";
+    wrapper.removeChild(topBar[0]);
+
+    //remove bottom bar
+    var content = document.getElementById('content');
+    content.removeChild(document.getElementById('sequence_footer'));
+
+    //fix iframes
+    var iframes = document.querySelectorAll('iframe');
+    //console.log(iframes.length);
+    //console.log(iframes[0].id);
+    //console.log(iframes[1]);
+    for(var i = 0; i < iframes.length; i++){
+        if(iframes[i].id != "instructure_ajax_error_result"){
+            iframes[i].style = "width: 100%; border: 0px; overflow: auto; height: 100%; min-height: 100%;"
+        }
+    };
+
+    //fix overflow
+    if(iframes.length > 1){
+        var doc_preview = document.getElementById('doc_preview');
+        console.log(doc_preview.childNodes[0].style);
+        console.log(doc_preview.childNodes[0]);
+        doc_preview.childNodes[0].style = "overflow: auto hidden; padding-right: 10px; resize: vertical; border: 1px solid transparent; height: 100%;";
+
+        //var get rid of redundant title
+        console.log(content.childNodes);
+        content.removeChild(content.childNodes[1]);
+    }
+
+    window.dispatchEvent(new Event('resize'));
+    //var get rid of 
+}
+fix();
--- popup.js
+++ popup.js
@@ -2,4 +2,68 @@
     chrome.tabs.query({active:true ,currentWindow:true},function(tabs){
         chrome.tabs.sendMessage(tabs[0].id,{todo:"changeColor"});
     });
+    //setup click listeners
+    document.getElementById('Off');
+    document.getElementById('Default').addEventListener('click', function(){
+        if(!document.getElementById('Default').checked){
+            var checkboxs = [];
+            for(var i = 0; i < 5;i++){
+                var div = document.createElement('div');
+                div.appendChild(document.createElement('input'));
+                div.children[0].type="checkbox";
+                checkboxs.push(div);
+            }
+            // set ids
+            checkboxs[0].children[0].id="TopBar";
+            checkboxs[1].children[0].id="BottomBar";
+            checkboxs[2].children[0].id="Enlarge";
+            checkboxs[3].children[0].id="SideBar";
+            checkboxs[4].children[0].id="Title";
+            // set labels
+            checkboxs[0].innerHTML += "Remove Top Bar";
+            checkboxs[1].innerHTML += "Remove Bottom Bar";
+            checkboxs[2].innerHTML += "Enlarge Previews";
+            checkboxs[3].innerHTML += "Remove Side Bar";
+            checkboxs[4].innerHTML += "Remove Redundant Title";
+            //create options
+            var options = document.createElement('h1');
+            options.innerText = "Options:";
+            document.getElementsByClassName('checkboxs')[0].appendChild(options);
+            for(var i = 0; i < checkboxs.length; i++){
+                checkboxs[i].children[0].checked=true;
+                document.getElementsByClassName('checkboxs')[0].appendChild(checkboxs[i]);
+            }
+        }
+        else{
+            var boxs = document.getElementsByClassName('checkboxs')[0];
+            for(var i = boxs.children.length; i > 2; i--){
+                boxs.removeChild(boxs.children[i-1]);
+            }
+        }
+    });
 })();
+
+// (function(){
+//         resetSettings.addEventListener('click',function(){
+//             preferences = {'WR':false,'CR':false,'HR':false,'ECO':false,'CORRUPT':false,'LOB':false,'ECONOMY':false,'ACONSUMER':false};
+//             var wr = document.getElementById('WR');
+//             if(wr){preferences.WR=wr.checked;};
+//             var cr = document.getElementById('CR');
+//             if(cr){preferences.CR=cr.checked;};
+//             var hr = document.getElementById('HR');
+//             if(hr){preferences.HR=hr.checked;};
+//             var eco = document.getElementById('ECO');
+//             if(eco){preferences.ECO=eco.checked;};
+//             var corrupt = document.getElementById('CORRUPT');
+//             if(corrupt){preferences.CORRUPT=corrupt.checked;};
+//             var lob = document.getElementById('LOB');
+//             if(lob){preferences.LOB=lob.checked;};
+//             var economy = document.getElementById('ECONOMY');
+//             if(economy){preferences.ECONOMY=economy.checked;};
+//             var aconsumer = document.getElementById('ACONSUMER');
+//             if(wr){preferences.ACONSUMER=aconsumer.checked;};
+//             chrome.storage.sync.set({'preferences':preferences});
+//         });
+//     }
+// })();
+
